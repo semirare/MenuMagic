c9e154e08c8f8608c9ea55bdfddfd53514aaf642
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2024-02-03 22:28:32 -0700

    redirect to recipes index on recipe create/update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -25,7 +25,7 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully created." }
+        format.html { redirect_to recipes_url, notice: "#{@recipe.name} was successfully created." }
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class RecipesController < ApplicationController
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." }
+        format.html { redirect_to recipes_url, notice: "#{@recipe.name} was successfully updated." }
         format.json { render :show, status: :ok, location: @recipe }
       else
         format.html { render :edit, status: :unprocessable_entity }
