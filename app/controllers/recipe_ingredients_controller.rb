135481789fac89708e74f1c9f2b137c0f92d98a5
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2024-02-10 00:04:53 -0700

    clean up formatting on recipe edit

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -6,16 +6,10 @@ class RecipeIngredientsController < ApplicationController
     respond_to do |format|
       if @recipe_ingredient.save
         format.html { redirect_to recipe_path(@recipe_ingredient.recipe) }
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.append("included_ingredients", partial: "recipes/included_ingredient",
-              locals: {recipe_ingredient: @recipe_ingredient}),
-            turbo_stream.remove("unused_ingredient_#{@recipe_ingredient.ingredient.id}")
-          ]
-        end
-        format.json { render :show, status: :created, location: @recipe_ingredient }
+        format.turbo_stream { render_create_turbo }
+        format.json { render :show, status: :created, location: @recipe_ingredient.recipe }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { redirect_to recipe_path(params[:recipe_id]), alert: 'There was an error saving the recipe ingredient.' }
         format.json { render json: @recipe_ingredient.errors, status: :unprocessable_entity }
       end
     end
@@ -28,20 +22,32 @@ class RecipeIngredientsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to recipe_path(@recipe_ingredient.recipe) }
-      format.turbo_stream do
-        render turbo_stream: [
-          turbo_stream.append("unused_ingredients", partial: "recipes/unused_ingredient",
-            locals: {ingredient: @recipe_ingredient.ingredient, recipe_id: @recipe_ingredient.recipe_id}),
-          turbo_stream.remove(@recipe_ingredient)
-        ]
-      end
+      format.turbo_stream { render_delete_turbo }
       format.json { head :no_content }
     end
   end
 
   private
-    # Only allow a list of trusted parameters through.
-    def recipe_ingredient_params
-      params.require(:recipe_ingredient).permit(:recipe_id, :ingredient_id)
-    end
+
+  # Only allow a list of trusted parameters through.
+  def recipe_ingredient_params
+    params.require(:recipe_ingredient).permit(:recipe_id, :ingredient_id)
+  end
+
+  def render_create_turbo
+    render turbo_stream: [
+      turbo_stream.append('included_ingredients', partial: 'recipes/included_ingredient',
+                                                  locals: {recipe_ingredient: @recipe_ingredient}),
+      turbo_stream.remove("unused_ingredient_#{@recipe_ingredient.ingredient.id}")
+    ]
+  end
+
+  def render_delete_turbo
+    render turbo_stream: [
+      turbo_stream.append('unused_ingredients', partial: 'recipes/unused_ingredient',
+                                                locals: {ingredient: @recipe_ingredient.ingredient,
+                                                         recipe_id: @recipe_ingredient.recipe_id }),
+      turbo_stream.remove(@recipe_ingredient)
+    ]
+  end
 end
