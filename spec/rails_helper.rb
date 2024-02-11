f56b403492ff530ef66fd1b98dc7bba47180ae4e
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2024-02-10 18:32:46 -0700

    add factory bot and faker

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,11 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
+require "factory_bot_rails"
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
