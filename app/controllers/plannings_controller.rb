acd061fbf795a79159377378121c8bc96a366828
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2024-02-17 21:18:51 -0700

    create grocery_list route

--- app/controllers/plannings_controller.rb
+++ app/controllers/plannings_controller.rb
@@ -10,17 +10,31 @@ class PlanningsController < ApplicationController
   end
 
   def create
-    puts(planning_params)
     @planning = Planning.new(planning_params)
 
     respond_to do |format|
       if @planning.save
-        format.html { redirect_to root_path }
+        format.html { redirect_to grocery_list_planning_path(@planning) }
         format.json { render json: @planning }
       else
         format.html { render :new, status: :unprocessable_entity, errors: @planning.errors }
         format.json { render json: @planning.errors }
       end
+    end
+  end
+
+  def grocery_list
+    @planning = Planning.find(params[:id])
+    @ingredients = @planning.recipes.includes(:ingredients).map(&:recipe_ingredients).flatten.group_by(&:ingredient_id)
+                            .map do |ingredient_id, recipe_ingredients|
+      quantity = recipe_ingredients.map(&:quantity).compact.sum # Remove nil values before summing
+      ingredient = Ingredient.find(ingredient_id)
+      unit = IngredientUnit.find(recipe_ingredients.first&.ingredient_unit_id)&.name || "N/A" # Fetch unit from IngredientUnits table
+      {
+        name: ingredient.name,
+        quantity:,
+        unit:
+      }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :recipe_ingredients
   resources :recipes
   resources :ingredients
-  resources :plannings
+  resources :plannings do
+    get "grocery_list", on: :member
+  end
 
   root "ingredients#index"
 end
